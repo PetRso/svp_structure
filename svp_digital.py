eded5f17d3de67ebf6bbd9c524941da801f8efaf
PetRso <76156555+PetRso@users.noreply.github.com>
2023-06-28 18:03:03 +0200

    Update svp_digital.py

--- svp_digital.py
+++ svp_digital.py
@@ -22,7 +22,7 @@ def format_definicia(text_orig):
     if '::' in text_orig:  # má definíciu napr. aktíva: ...
         text_orig = text_orig.split(':')  # definicia ma v sebe typ napr. mat reprezentacie
         typ = text_orig[0].strip()
-        text += f'######     {typ.capitalize()}\n'  # typ definicie
+        text += f'###### &nbsp;&nbsp;&nbsp;&nbsp;{typ.capitalize()}\n'  # typ definicie
         text_orig = text_orig[1]  # definicia # TODO test, existuje iba jedna :
     text_orig = text_orig.split(';')   # viac standardov v jednom poly
     text_orig = [x.strip() for x in text_orig]
